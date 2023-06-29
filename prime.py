e0fe80684822ae60e2af5b2e23efc0bca8e4afc4
Thiruselvan123 <123719355+Thiruselvan123@users.noreply.github.com>
2023-06-29 22:38:12 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
            #To find prime number or not using functions#
 def prime_number(N,flag):        #fuction defintion 
-    for i in range(2,N):         #reason we choose upto N//2 is all factor of N always belongs to its half
+    for i in range(2,N):         #reason we choose 2 is bcoz 1 is neiher prime nor composite
         if(N%i==0):              #check whether the number is a divided with other factor.
             flag=1
     return flag
